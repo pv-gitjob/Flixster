fecdacdfe1a035090f2771f74048677fe254ede6
pv-gitjob <58917968+pv-gitjob@users.noreply.github.com>
2020-01-18 15:12:21 -0800

    Add files via upload

---
+++ Flixster/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flixster
+//
+//  Created by Praveen V on 1/16/20.
+//  Copyright © 2020 Praveen V. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
